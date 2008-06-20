5246d45aa7c1c607409af4465671b1d92870dd6f
Greg Fitzgerald <netzdamon@gmail.com>
2008-06-19 20:30:03 -0400

    capitalize after - and [0-9]

--- cleanmusic.rb
+++ cleanmusic.rb
@@ -1,12 +1,39 @@
 #!/usr/bin/ruby -w
 
+#== Synopsis
+#
+# cleanmusic: Removes uwanted files, Renames directories and files in a logical manor.
+#
+# = Usage
+#
+# cleanmusic [OPTION] ... SRCDIR
+# -h --help
+#   show help
+#
+# -s --srcdir </storage/music/>
+#  Top level directory to where files/directories are located 
+#
+# -v --version
+#   Display current version
+
+require 'getoptlong'
+require 'rdoc/usage'
+
+
+#TODO Refactor gsubs into a function?
+#TODO Verbose flag
+#TODO cp instead of mv as option
+#TODO option to specify dstdir
+#TODO rename dstdir to srcdir
 class Rename 
   attr_accessor :dname 
   
   def initialize(dname)  
        @dname = dname
   end  
-  
+ 
+  #TODO Rename directories only
+  #TODO Make Recursive 
   def dren
     dirs = Dir.entries(@dname)
     dirs.each do |d|
@@ -14,32 +41,62 @@ class Rename
       oldDir = @dname + d
       d = d.gsub(/^[a-z]|\s+[a-z]/) { |a| a.upcase.strip.to_s }
       d = d.gsub(/^[a-z]|\_+[a-z]/) { |a| a.upcase.strip.to_s }
+      d = d.gsub(/^[a-z]|\-+[a-z]/) { |a| a.upcase.strip.to_s }
+      d = d.gsub(/^[0-9]|\s+[0-9]/) { |a| a.upcase.strip.to_s }
       d = d.gsub(" ", "")
       d = d.gsub("_", "")
       d = d.gsub("!", "")
       d = d.gsub("?", "")
-      File.rename(oldDir, d)
+      File.rename(oldDir, @dname + d)
     end
   end
 
+  #TODO Rename files only
+  #TODO Make Recursive 
   def fren
     files = Dir.entries(@dname)
     files.each do |f|
       next if f == "." or f == ".."
       oldFile = @dname + f
       f = f.gsub(/^[a-z]|\s+[a-z]/) { |a| a.upcase.strip.to_s }
+      f = f.gsub(/^[0-9]|\-+[0-9]/) { |a| a.upcase.strip.to_s }
       f = f.gsub(/^[a-z]|\_+[a-z]/) { |a| a.upcase.strip.to_s }
+      f = f.gsub(/^[a-z]|\-+[a-z]/) { |a| a.upcase.strip.to_s }
       f = f.gsub(/-{2,}/, "")
       f = f.gsub(" ", "")
       f = f.gsub("_", "")
       f = f.gsub("?", "")
       f = f.gsub("!", "")
-      File.rename(oldFile, f)
+      File.rename(oldFile, @dname + f)
     end
- 
+  end
+
+  def clean
+    #TODO recursivly remove certain file types, txt, jpg, nfo, sfv
+  end
+
+end
+
+opts = GetoptLong.new(
+      [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+      [ '--version', '-v', GetoptLong::NO_ARGUMENT ],
+      [ '--srcdir', '-s', GetoptLong::REQUIRED_ARGUMENT ]
+
+    )
+
+dstdir = nil 
+opts.each do |opt, arg|
+  case opt
+    when '--help'
+      RDoc::usage
+      exit 1
+    when '--srcdir'
+      srcdir = arg
+    when '--version'
+      puts "Version: 0.1"
+      exit 1
   end
 end
 
-foo = Rename.new("/home/gregf/test/")
-foo.dren
-foo.fren
+music = Rename.new(srcdir)
+music.dren
