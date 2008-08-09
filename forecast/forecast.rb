da60e7e553a498bc5410565386b91bfab59eff70
Greg Fitzgerald <netzdamon@gmail.com>
2008-08-09 12:59:21 -0400

    Changed wording

--- forecast/forecast.rb
+++ forecast/forecast.rb
@@ -13,12 +13,12 @@ end
 forecast = service.fetch_forecast("USME0249", 5)
 puts "Location: %s" % forecast.location_name
 puts
-puts "Current Temperature: %s" % forecast.current.temperature
-puts "Current FLIK: %s" % forecast.current.flik
-puts "Current Windspeed: %s" % forecast.current.wind.speed + " Gust " + forecast.current.wind.gust
-puts "Current Dewpoint: %s" % forecast.current.dewp
-puts "Current Humidity: %s" % forecast.current.hmid
-puts "Current Visibility: %s" % forecast.current.vis
+puts "Temperature: %s" % forecast.current.temperature
+puts "Feels Like: %s" % forecast.current.flik
+puts "Windspeed: %s" % forecast.current.wind.speed + " Gust " + forecast.current.wind.gust
+puts "Dewpoint: %s" % forecast.current.dewp
+puts "Humidity: %s" % forecast.current.hmid
+puts "Visibility: %s" % forecast.current.vis
 puts 
 puts "Tomorrow's High: %s" % forecast.tomorrow.high
 puts "Tomorrow's Low: %s" % forecast.tomorrow.low
