df2f230124b0acf7bacc257c8042a8be65d6231b
Greg Fitzgerald <netzdamon@gmail.com>
2008-08-29 00:58:02 -0400

    Cleanups to mysqlgit.sh

--- mysql_backup/mysqlgit.sh
+++ mysql_backup/mysqlgit.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# --lock-all-tables < use this on final backups
-# need to create a .my.cnf file in your home directory to set mysql user/pass.
-#
-# .my.cnf example
-# [client]
-# user = backups
-# password = somepasshere
-#
-# Run via crontab
-#
-# Every 3 hours
-#
-# 00 0-23/3 * * * /path/to/mysqlgit.sh
-#
-
-
 #
 # Edit These
 #
@@ -28,29 +12,30 @@ GROUP="gregf"
 NOW=$(date +"%m-%d-%Y %I:%M%p")
 
 # cd to the script dir to make sure files are put here
+# --lock-all-tables < use this on final backups
 cd ${BACKUP}
- 
+
 DATABASES="$(mysql -h ${HOSTNAME} -Bse 'SHOW DATABASES')"
- 
+
 # loop through the databases
 for DB in ${DATABASES[@]}
-do  
-    mkdir -p "$DB"  
+do
+    mkdir -p "$DB"
     TABLES="$(mysql -h ${HOSTNAME} $DB -Bse 'SHOW TABLES')"
     # loop through the tables in the database
     for TABLE in ${TABLES[@]}
     do
-        nice -n 19 mysqldump --compress --add-locks -h ${HOSTNAME} "$DB" "$TABLE" > "${DB}/${TABLE}.sql"
+        mysqldump --compress --add-locks -h ${HOSTNAME} "$DB" "$TABLE" > "${DB}/${TABLE}.sql"
     done
 done
 
 #
 # version control
 #
-
+nice -n19 $(
 ionice -c3 git add .
 ionice -c3 git commit -m "cron backup ${NOW}"
-
+)
 #
 # Set sane permissions
 #
