75fa1240c93b1660e4c3b69f9697a03ff71d29d7
Greg Fitzgerald <netzdamon@gmail.com>
2008-08-31 17:58:29 -0400

    Add more options, verbose, --dotfiles, --servers, --location

--- dotfiles/dotfiles.rb
+++ dotfiles/dotfiles.rb
@@ -13,25 +13,41 @@
 #
 # -v --version
 #   Display current version
+#
+# -d --dotfiles file1 file2 file3
+#   Override the default configuration via command line.
+#
+# -s --servers server1 server2 server
+#   Override the default configuration via command line.
+#
+# -l --location /some/path
+#   Override the default location of the dotfiles.
 
 # Edit these
-dotfiles = %w[.zshrc .vimrc .zsh .vim .dir_colors .zkbd]
-servers = %w[gila web miker ibiblio nlfg isp4cheap]
+$dotfiles = %w[.zshrc .vimrc .zsh .vim .dir_colors .zkbd]
+$servers = %w[gila web miker ibiblio nlfg isp4cheap]
+$location = "/home/gregf"
+$verbose = true
 
 #TODO Allow user to override dotfiles and servers array via cmd line opt.
-#TODO Verbose option, no verbosity by default (for cron)
 #TODO Maybe use benchmark and time the process via optional parameter
-#TODO Use getoptlong
 
 require 'getoptlong'
 require 'rdoc/usage'
 require 'net/scp'
 
-def send_defaults
-  list_dotfiles = dotfiles.join(", ")
-  servers.each do |server|
-      puts "Transfering #{list_dotfiles} to #{server}"
-      dotfiles.each do |dotfile|
+def verbose(list_dotfiles, server)
+  if $verbose
+    puts "Transfering #{list_dotfiles} to #{server}"
+  end
+end
+
+def send_defaults()
+  Dir.chdir($location)
+  list_dotfiles = $dotfiles.join(", ")
+  $servers.each do |server|
+      verbose(list_dotfiles, server)
+      $dotfiles.each do |dotfile|
         Net::SCP.start("#{server}", :user) do |scp|
           scp.upload! "#{dotfile}", "~", :recursive => true
       end
@@ -39,15 +55,17 @@ def send_defaults
   end
 end
 
-opts = GetoptLong.new(
-      [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
-      [ '--version', '-v', GetoptLong::NO_ARGUMENT ]
-    )
-
-
 if ARGV.empty?
   send_defaults
 end
+
+opts = GetoptLong.new(
+      [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+      [ '--version', '-v', GetoptLong::NO_ARGUMENT],
+      [ '--dotfiles', '-d', GetoptLong::REQUIRED_ARGUMENT],
+      [ '--servers', '-s', GetoptLong::REQUIRED_ARGUMENT],
+      [ '--location', '-l', GetoptLong::REQUIRED_ARGUMENT]
+    )
 
 opts.each do |opt, arg|
   case opt
@@ -57,5 +75,13 @@ opts.each do |opt, arg|
     when '--version'
       puts "Version: 0.1"
       exit 1
+    when '--dotfiles'
+      $dotfiles = arg.to_a
+    when '--servers'
+      $servers = arg.to_a
+    when '--location'
+      $location = arg.to_s
   end
 end
+
+send_defaults
