4ec1c0e2e05978eb407172e2a14de0f217d9b907
Greg Fitzgerald <netzdamon@gmail.com>
2008-09-01 01:04:38 -0400

    Start of shot.rb

--- shot/shot.rb
+++ shot/shot.rb
@@ -7,3 +7,24 @@
 #TODO scp to server
 #TODO if directories on server do not exist create them.
 #TODO cmd line options, fullscreen, select, naming convention of image
+
+require 'getoptlong'
+require 'configatron'
+require 'net/ssh'
+require 'net/scp'
+
+### EDIT These ###
+configatron do |config|
+  config.scrot = "/usr/bin/scrot"
+  config.server = "gregf.org"
+  config.remote_path = "~/blog/shared/stuff/screenshots/"
+  # If these are left blank it will read settings from ~/.ssh/config
+  config.user = "gregf"
+  config.port = "159"
+  # Set this to anything you want.
+  config.tmpdir = ENV["TMP"]
+  config.clean_tmpdir = true
+end
+
+
+system(configatron.scrot)
