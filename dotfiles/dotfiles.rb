b2bd14de4a3feb1b2e1e374e873c0bdbcd7472a9
Greg Fitzgerald <netzdamon@gmail.com>
2008-08-31 22:41:56 -0400

    Majo rewrite of getopt's. Needed to use another system because of limiations of getoptlong

--- dotfiles/dotfiles.rb
+++ dotfiles/dotfiles.rb
@@ -1,39 +1,15 @@
 #!/usr/bin/ruby
 
-#== Synopsis
-#
-# dotfiles: Quick way to sync your dotfiles to multiple servers.
-#
-#
-# = Usage
-#
-# dotfiles [OPTION] ...
-# -h --help
-#   show help
-#
-# -v --version
-#   Display current version
-#
-# -d --dotfiles file1 file2 file3
-#   Override the default configuration via command line.
-#
-# -s --servers server1 server2 server
-#   Override the default configuration via command line.
-#
-# -l --location /some/path
-#   Override the default location of the dotfiles.
-
 # Edit these
 $dotfiles = %w[.zshrc .vimrc .zsh .vim .dir_colors .zkbd]
 $servers = %w[gila web miker ibiblio nlfg isp4cheap]
 $location = "/home/gregf"
-$verbose = true
+$verbose = false
 
-#TODO Allow user to override dotfiles and servers array via cmd line opt.
+#TODO Rescue from some possible errors.
 #TODO Maybe use benchmark and time the process via optional parameter
 
-require 'getoptlong'
-require 'rdoc/usage'
+require 'Getopt/Declare'
 require 'net/scp'
 
 def verbose(list_dotfiles, server)
@@ -42,7 +18,7 @@ def verbose(list_dotfiles, server)
   end
 end
 
-def send_defaults()
+def sync_dotfiles
   Dir.chdir($location)
   list_dotfiles = $dotfiles.join(", ")
   $servers.each do |server|
@@ -56,32 +32,28 @@ def send_defaults()
 end
 
 if ARGV.empty?
-  send_defaults
+  sync_dotfiles
 end
 
-opts = GetoptLong.new(
-      [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
-      [ '--version', '-v', GetoptLong::NO_ARGUMENT],
-      [ '--dotfiles', '-d', GetoptLong::REQUIRED_ARGUMENT],
-      [ '--servers', '-s', GetoptLong::REQUIRED_ARGUMENT],
-      [ '--location', '-l', GetoptLong::REQUIRED_ARGUMENT]
-    )
+specification = %q(
+  [tight]
 
-opts.each do |opt, arg|
-  case opt
-    when '--help'
-      RDoc::usage
-      exit 1
-    when '--version'
-      puts "Version: 0.1"
-      exit 1
-    when '--dotfiles'
-      $dotfiles = arg.to_a
-    when '--servers'
-      $servers = arg.to_a
-    when '--location'
-      $location = arg.to_s
-  end
-end
+  -server <server>...      Process named file(s)
+                              { $servers = server.to_a }
+  -s <server>...     [ditto]
 
-send_defaults
+  -dotfile <dotfile>...    Process named file(s)
+                              { $dotfiles = dotfile.to_a }
+  -d  <dotfile>...    [ditto]
+
+  -location  <location>     Process location
+                              { $location = location }
+  -l <location>       [ditto]
+
+  --verbose                 Verbose
+                             { $verbose = true }
+  )
+
+
+args = Getopt::Declare.new(specification)
+sync_dotfiles
