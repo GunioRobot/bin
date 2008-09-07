ae36e95fe9740a290fdc9be933036fa0d9447bb1
Greg Fitzgerald <netzdamon@gmail.com>
2008-09-06 22:43:00 -0400

    add some spaces in front of the output, makes my conkyrc output nicer

--- forecast/forecast.rb
+++ forecast/forecast.rb
@@ -11,17 +11,17 @@ def mylocal(city)
 end
 
 forecast = service.fetch_forecast("USME0249", 2)
-puts "Temperature: %s" % forecast.current.temperature
-puts "Feels Like: %s" % forecast.current.flik
-puts "Windspeed: %s" % forecast.current.wind.speed + " Gust " + forecast.current.wind.gust
-puts "Dewpoint: %s" % forecast.current.dewp
-puts "Humidity: %s" % forecast.current.hmid
-puts "Visibility: %s" % forecast.current.vis
-puts "Outlook: %s" % forecast.current.outlook
+puts "\sTemperature: %s" % forecast.current.temperature
+puts "\sFeels Like: %s" % forecast.current.flik
+puts "\sWindspeed: %s" % forecast.current.wind.speed + " Gust " + forecast.current.wind.gust
+puts "\sDewpoint: %s" % forecast.current.dewp
+puts "\sHumidity: %s" % forecast.current.hmid
+puts "\sVisibility: %s" % forecast.current.vis
+puts "\sOutlook: %s" % forecast.current.outlook
 puts
-puts "Tommorow:"
+puts "\sTommorow:"
 puts
-puts "High: %s" % forecast.tomorrow.high
-puts "Low: %s" % forecast.tomorrow.low
-puts "Outlook: %s" % forecast.tomorrow.outlook
-puts "Chance of Precipitation: %s" % forecast.tomorrow.ppcp
+puts "\sHigh: %s" % forecast.tomorrow.high
+puts "\sLow: %s" % forecast.tomorrow.low
+puts "\sOutlook: %s" % forecast.tomorrow.outlook
+puts "\sChance of Precipitation: %s" % forecast.tomorrow.ppcp
