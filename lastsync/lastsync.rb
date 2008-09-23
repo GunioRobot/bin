c45320e28d9181b80f8193fbda4066ad509c8752
Greg Fitzgerald <netzdamon@gmail.com>
2008-09-22 21:19:28 -0400

    initial import of lastsync.rb

---
+++ lastsync/lastsync.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+require 'date'
+
+LOG_FILE="/var/log/paludis.log"
+SYNC_PATTERN="finished sync of repository gentoo"
+sync_date = %w[]
+if File.file?(LOG_FILE)
+  File.open(LOG_FILE, "r") do |f|
+    f.each_line do |line|
+      line = line.grep( /#{SYNC_PATTERN}/ ) do |line|
+        sync_date.push("#{line} ")
+      end
+    end
+  end
+end
+
+lastline = sync_date.last.gsub("\n", "")
+unixtime = Time.at(lastline.to_i)
+puts unixtime.strftime("%a, %b %d @ %I:%M%P")
