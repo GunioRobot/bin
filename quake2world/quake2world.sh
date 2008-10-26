894178e6243d133c57190d6ce77e70cdea71138e
Greg Fitzgerald <netzdamon@gmail.com>
2008-10-26 15:30:28 -0400

    initial import of quake2world installer script

---
+++ quake2world/quake2world.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+SUBVERSION_URL="svn://jdolan.dyndns.org/quake2world"
+RSYNC_URL="rsync://satgnu.net/quake2world quake2world"
+RSYNC_OPTS="-avz --delete"
+SUDO="/usr/bin/sudo"
+SVN="/usr/bin/svn"
+RSYNC="/usr/bin/rsync"
+BUILD_LOCATION="${TMP}/quakeworld/"
+
+if [[ -d ${BUILD_LOCATION} ]]; then
+    rm -rf ${BUILD_LOCATION}
+else
+    mkdir -p ${BUILD_LOCATION}
+fi
+
+function compile {
+    cd ${BUILD_LOCATION}
+    ${SVN} co ${SUBVERSION_URL} quake2world
+    cd quake2world
+    autoreconf -i
+    ./configure
+    make 
+    ${SUDO} make install
+}
+
+function get_maps {
+    cd /usr/local/share
+    ${SUDO} ${RSYNC} ${RSYNC_OPTS} ${RSYNC_URL}
+}
+
+function cleanup {
+    if [ -d ${BUILD_LOCATION} ]; then
+        rm -rf ${BUILD_LOCATION}
+    fi
+}
+
+compile
+get_maps
+cleanup
--- runbackup/runbackup.sh
+++ runbackup/runbackup.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 # Run this script at idle priority
-nice -n19 $(
-    ionice -c3 /usr/bin/nice -n 15 /usr/bin/rsnapshot -c /etc/rsnapshot/rsnapshot.conf daily
-)
+ionice -c3 /usr/bin/nice -n 15 /usr/bin/rsnapshot -c /etc/rsnapshot/rsnapshot.conf daily
