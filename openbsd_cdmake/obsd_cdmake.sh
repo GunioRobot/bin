251fbc17f75c9248fec8c45d3022b2313f6be50f
Greg Fitzgerald <netzdamon@gmail.com>
2008-11-01 19:03:10 -0400

    update cd making process for later releases

--- openbsd_cdmake/obsd_cdmake.sh
+++ openbsd_cdmake/obsd_cdmake.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
 
 MIRROR="ftp.openbsd.org"
-VERSION="4.3"
-CDVER="43"
+VERSION="4.4"
+CDVER="44"
 ARCH="i386"
 
 mkdir OpenBSD
-mkdir OpenBSD/$VERSION
-cd OpenBSD/$VERSION
+cd OpenBSD
 
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/ports.tar.gz
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/src.tar.gz
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/sys.tar.gz
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/XF4.tar.gz
-
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/HARDWARE
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/PACKAGES
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/PORTS
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/README
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/ftplist
-wget -N ftp://${MIRROR}/pub/OpenBSD/${VERSION}/root.mail
-
-wget -r -nH -N -nd ftp://${MIRROR}/pub/OpenBSD/${VERSION}/Changelogs/ -P Changelogs
-wget -r -nH -N -nd ftp://${MIRROR}/pub/OpenBSD/${VERSION}/${ARCH}/ -P ${ARCH}
-
-cd ..
-mkisofs -v -r -T -l -L -J -V "OpenBSD ${VERSION}" -b ${VERSION}/${ARCH}/cdrom${CDVER}.fs -c boot.catalog -o ~/openbsd${CDVER}.iso -A "OpenBSD ${VERSION} Install" .
+wget -r -np ftp://${MIRROR}/pub/OpenBSD/${VERSION}/${ARCH}
+mv ${MIRROR}/pub/OpenBSD/${VERSION} .
+cp ${VERSION}/${ARCH}/cd${CDVER}.iso OpenBSD-${VERSION}.iso
+growisofs -M OpenBSD-${VERSION}.iso -R -iso-level 3 -graft-points ${VERSION}=${VERSION}
+rm -rf ${MIRROR} 
 
 echo
 echo "Your OpenBSD ${VERSION} CD is finished! To burn the CD, run:"
-echo "cdrecord -v speed=48 dev=0,0,0 -data OpenBSD.iso"
 echo
