86e54298c83398389ea42dd459a63ea8612db850
Greg Fitzgerald <netzdamon@gmail.com>
2009-01-16 22:58:32 -0500

    Changed FEH to WALLPAPER_CMD Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- wallpaper/wallpaper.rb
+++ wallpaper/wallpaper.rb
@@ -3,8 +3,8 @@
 
 WALLPAPER="/home/gregf/.wallpaper/" # Full path no ~
 IMAGE_FORMATS="[.jpg,.gif,.png.,bmp]" # Keep this a string
-FEH="/usr/bin/feh"
-FEH_OPTIONS="--bg-scale"
+WALLPAPER_CMD="/usr/bin/feh"
+WPCMD_OPTIONS="--bg-scale"
 
 unless IMAGE_FORMATS.class == String
   puts "IMAGE_FORMATS must be a string, surround it in quotes."
@@ -16,13 +16,13 @@ unless File.directory?("#{WALLPAPER}")
   exit
 end
 
-unless File.executable?("/usr/bin/feh")
-  puts "#{FEH} is not executable"
+unless File.executable?("#{WALLPAPER_CMD}")
+  puts "#{WALLPAPER_CMD} is not executable"
   exit
 end
 
 Dir.chdir("#{WALLPAPER}") do
   files = Dir.glob("*#{IMAGE_FORMATS}")
   random_file = rand(1-files.size).to_i
-  IO.popen("#{FEH} #{FEH_OPTIONS} #{files[random_file]}")
+  IO.popen("#{WALLPAPER_CMD} #{WPCMD_OPTIONS} #{files[random_file]}")
 end
