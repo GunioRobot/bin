e9d902262b82ef9fa72f9f243c3f1e7aff472b1e
Greg Fitzgerald <netzdamon@gmail.com>
2009-02-08 21:18:20 -0500

    Initial import of gmail.rb
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

---
+++ gmail/gmail.rb
@@ -0,0 +1,96 @@
+#!/usr/bin/env ruby
+# gmail checker based off Brian Carpers code @
+# http://briancarper.net/conky/check_mail.rb
+#
+# Modified by Greg Fitzgerald <netzdamon@gmail.com>
+
+%w{rubygems daemons net/imap time}.each { |lib| require lib } 
+
+#TODO strip html from verbose messages
+#TODO make this use a class instead of purely functional
+#TODO probably shouldn't display anything if count is 0?
+#TODO ideas?
+
+# Edit these
+@username = "username@gmail.com"
+@password = "yourpasword"
+@mailboxes = %w[INBOX family friends] #Takes an array of mailboxes you want to monitor
+@imapserver = "imap.gmail.com"
+@imapport = "993" 
+@ssl = true # enable ssl?
+@limit = 5 # number of messages to display per inbox
+@image = "/home/gregf/gmail3.png" # path to gmail image or supply your own
+@display = ":0.0" # xorg display needed to run in the background
+@wait = 600 # Time to wait beetween checks in seconds
+@notify = "/usr/bin/notify-send" # path to notify-send
+@urgency = "low" # notify-send urgency level low|normal|critical
+@strftime = "%I:%M %p" # date format
+@verbose = false 
+
+def message(msg)
+  if @verbose
+    $stderr.puts %Q{ #{msg} }
+  end
+end
+
+def checkmail
+  ENV["DISPLAY"] = @display
+  imap = Net::IMAP.new("#{@imapserver}", "#{@imapport}", @ssl)
+  message "Connecting to #{@imapserver} #{@imapport}"
+  imap.login(@username, @password)
+  message "Using #{@username} and password (hidden)"
+  @mailboxes.each do |mbox|
+    message "Checking for mail in: #{mbox}"
+    imap.examine("#{mbox}")
+    data = []
+    count = 0
+      imap.search(['UNSEEN']).each do |message_id|
+          env = imap.fetch(message_id, 'ENVELOPE')[0].attr['ENVELOPE']
+
+          # Filter here, if you want.
+          next if /SPAM/i.match(env.subject)
+
+          count += 1
+
+          # Limit to number of new messages 
+          if count <= @limit then
+              if env.sender[0].mailbox then
+                  addr = '&lt;' + env.sender[0].mailbox + '@' + env.sender[0].host + '&gt;'
+              end
+              if (name = env.sender[0].name).nil? then
+                  name = ''
+              end
+              if (subject = env.subject).nil? then
+                  subject = ''
+              end
+              # Let's limit the length of the subject.
+              subject.sub!(/(.{25}).*/, '\1...')
+              data.push name + ":" + addr + ":" + subject
+          end
+      end
+    newmail = "<b>#{count}</b> new messages in <b>#{mbox}</b> as of #{Time.now.strftime(@strftime)}"
+    message newmail
+    %x["/usr/bin/notify-send" "-u" "normal" "-i" "#{@image}" "#{newmail}"]
+    data.each do |email|
+      email = email.split(":")
+      name = email[0]
+      addr = email[1]
+      subject = email[2]
+      message "\t#{name} #{addr} - #{subject}"
+      %x["#{@notify}" "-u" "#{@urgency}" "#{name} - #{addr}"]
+      unless subject.nil?
+        %x["#{@notify}" "-u" "#{@urgency}" "\t#{subject}"]
+      end
+    end
+  end
+end
+
+
+Daemons.run_proc("#{__FILE__}") do
+  loop do
+    message "Firing up daemon and checking mail"
+    checkmail()
+    message "sleeping for #{@wait} seconds"
+    sleep(@wait)
+  end
+end
